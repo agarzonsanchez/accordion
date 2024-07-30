6c2bf3122d9540d092a049fb6e907b691b6c6221
Andres Garzon <agarzonsanchez90@gmail.com>
2024-07-30 17:17:38 -0400

    styles and multiselection button

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
 import data from "../../data";
+import "./styles.css";
 
 export default function Accordion() {
   const [index, setIndex] = useState();
+  const [selectMultiple, setSelectMultiple] = useState(false);
 
   function handleOnClickIndex(itemIndex) {
-    setIndex(itemIndex);
+    index === itemIndex ? setIndex(null) : setIndex(itemIndex);
   }
+
+  function enableMultiSelection() {}
   return (
     <div>
-      <div>
+      <div className="accordion-container">
+        <button onClick={enableMultiSelection}>
+          Select mutiple:{selectMultiple ? "enable" : "disable"}{" "}
+        </button>
         {data.map((item) => (
-          <div key={item.id} onClick={() => handleOnClickIndex(item.id)}>
+          <div
+            className="button-item"
+            key={item.id}
+            onClick={() => handleOnClickIndex(item.id)}
+          >
             <h1>{item.question}</h1>
             {index === item.id ? <p>{item.answer}</p> : null}
           </div>
